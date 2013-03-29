e9181cc99d2f9beae88e28a5816394829e3047e3
Bruno Rocha <rochacbruno@gmail.com>
2013-03-29 00:58:01 +0000

    Fix template multi site  #5

--- opps/polls/views.py
+++ opps/polls/views.py
@@ -19,20 +19,12 @@ class PollList(ListView):
 
     @property
     def template_name(self):
-        homepage = Channel.objects.get_homepage(site=self.site)
-        if not homepage:
-            return None
-
-        long_slug = self.kwargs.get('channel__long_slug',
-                                    homepage.long_slug)
-        if homepage.long_slug != long_slug:
-            long_slug = long_slug[:-1]
 
         domain_folder = 'polls'
         if self.site.id > 1:
             domain_folder = "{0}/polls".format(self.site)
 
-        return '{0}/{1}.html'.format(domain_folder, long_slug)
+        return '{0}/poll_list.html'.format(domain_folder)
 
     @property
     def queryset(self):
