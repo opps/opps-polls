54e8f7fe33319bf30f217079920a8cafe56c2182
Ellison Leao <ellisonleao@gmail.com>
2013-07-18 17:28:34 -0300

    Add travis file and change some makefile entries

--- Makefile
+++ Makefile
@@ -1,8 +1,45 @@
+.PHONY: test
+test: pep8
+	python runtests.py
+
+.PHONY: travis
+travis:
+	pip install -r requirements_test.txt --use-mirrors
+	export OPPS_TRAVIS=True
+	python setup.py develop
+
+.PHONY: pep8
+pep8:
+	@flake8 . --ignore=E501,F403,E126,E127,E128,E303 --exclude=migrations
+
+.PHONY: sdist
+sdist: test
+	@python setup.py sdist upload
+
+.PHONY: clean
+clean:
+	@find ./ -name '*.pyc' -exec rm -f {} \;
+	@find ./ -name 'Thumbs.db' -exec rm -f {} \;
+	@find ./ -name '*~' -exec rm -f {} \;
 
 .PHONY: install
 install:
 	pip install -r requirements.txt --use-mirrors
 	python setup.py develop
+
+.PHONY: makemessages
+makemessages:
+	echo "making messages";\
+	cd opps/polls;\
+	django-admin.py makemessages -l en_US;\
+	cd ../../;\
+
+.PHONY: compilemessages
+compilemessages:
+	echo "compiling messages";\
+	cd opps/ganalytics;\
+	django-admin.py compilemessages;\
+	cd ../../;\
 
 .PHONY: tx
 tx:
---
+++ runtests.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import sys
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+        MIDDLEWARE_CLASSES=(
+            'django.middleware.common.CommonMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'django.contrib.redirects.middleware.RedirectFallbackMiddleware'
+        ),
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.admin',
+            'django.contrib.redirects',
+
+            'opps.core',
+            'opps.channels',
+            'opps.boxes',
+            'opps.sources',
+            'opps.articles',
+            'opps.images',
+            'opps.sitemaps',
+            'opps.containers',
+            'opps.archives',
+            'opps.flatpages',
+
+            'opps.polls',
+            'taggit',
+
+        ),
+        SITE_ID = 1,
+        ROOT_URLCONF = "opps.urls",
+        TEST_RUNNER = 'django_coverage.coverage_runner.CoverageRunner',
+        HAYSTACK_CONNECTIONS = {
+            'default': {
+                'ENGINE': 'haystack.backends.simple_backend.SimpleEngine'
+            }
+        }
+    )
+
+
+def runtests():
+    argv = [sys.argv[0], 'test', 'polls']
+    execute_from_command_line(argv)
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    runtests()
