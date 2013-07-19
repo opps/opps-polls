301ea81a1ffa622bb8ba2384e0849dd5497ce0af
Ellison Leao <ellisonleao@gmail.com>
2013-07-19 10:58:20 -0300

    Add opps>=0.2 in setup install_requires

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 from opps import polls
 
 
-install_requires = ["opps"]
+install_requires = ["opps>=0.2"]
 
 classifiers = ["Development Status :: 4 - Beta",
                "Intended Audience :: Developers",
