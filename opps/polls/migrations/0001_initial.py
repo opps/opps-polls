6d22e0010c76bc0974829bd8535d418c35f357ec
Davi Campos <arlocm@gmail.com>
2013-08-18 00:16:48 -0300

    fixing migrations dependencies to use postgresql

--- opps/polls/migrations/0001_initial.py
+++ opps/polls/migrations/0001_initial.py
@@ -10,6 +10,12 @@ User = get_user_model()
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("images", "0001_initial"),
+        ("articles", "0001_initial"),
+        ("containers", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'Poll'
         db.create_table(u'polls_poll', (
